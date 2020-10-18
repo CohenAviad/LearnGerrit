645a6a3e470de9703032eb9c151ee9c6582dc4e1
Arye Shemesh <arye@zadarastorage.com>
2020-10-18 12:27:18 +0300

    new js 2
    Change-Id: Ic03b65fb7bc72533fd0e5e9f666e7741857e9268

--- new.js
+++ new.js
@@ -1,9 +1,67 @@
-aaaa
-dsv
-eertrdbfsadffdddeva
-dsfsdfa
-dsfsdfa
-dsfsdfa
-dsfsdfa
-dsfsdfa
-dsfsdfa
+/* Config-
+ * store: the created store for the chart (usually shared with other charts)
+ * type: the type of chart to create
+ * title: the title of the left column
+ * fields: the fields to measure on the left column
+ */
+Ext.define('Vpsa.view.performance.charts.FlcBarChart', {
+  alias: 'widget.flc-bar-chart',
+  extend: 'Vpsa.view.performance.charts.BaseFlcChart',
+  itemId: 'flc-bar-chart', // Needs to be identical to Base.js in order for the canvas to find it for refreshCanvas
+  drawChart: function (data) {
+
+    chart_series = []
+    data_arr = []
+    categories_arr = []
+    data_keys = Object.keys(data)
+    // data_keys.sort((a, b) => (data[a]["Order"] - data[b]["Order"]))
+    // for (var key of data_keys) {
+    //   data_arr.push({
+    //     name: key,
+    //     y: this.getDataValue(data[key]["Capacity"])
+    //   });
+    //   categories_arr.push(key);
+    // }
+    // chart_series.push({
+    //   data: data_arr
+    // });
+
+    Highcharts.chart(this.getChartDivId(), {
+      series: chart_series,
+      chart: {
+        type: 'bar'
+      },
+      legend: {
+        enabled: false
+      },
+      title: {
+        text: ''
+      },
+      xAxis: {
+        categories: categories_arr
+      },
+      yAxis: {
+        allowDecimals: false,
+        title: {
+          text: 'Aggregated Capacity (TiB)'
+        }
+      },
+      tooltip: {
+        formatter: function () {
+          return '<b>' + this.point.name + '</b><br/>' +
+            this.point.y + ' TiB';
+        }
+      },
+      plotOptions: {
+
+        bar: {
+          dataLabels: {
+            enabled: true,
+            format: "{y} TiB"
+          }
+        }
+      }
+    });
+  }
+})
+
